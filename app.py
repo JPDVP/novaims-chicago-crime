433cae799454a52b2b28fd127c08c03cca62256a
jpdvp <joaopdvpereira@outlook.com>
2020-05-18 22:22:08 +0100

    Initial app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.graph_objs as go
+
+
+app =  dash.Dash()
+
+app.layouyt = html.Div([
+    'Test'
+])
+
+
+if __name__ == '__main__':
+    app.run_server()
